e010f0b21fec9c9cc4245a33bd42d1c9e4f59742
mgwalker <mgwalker@gmail.com>
2015-09-12 19:57:50 -0500

    Add cooger-style and pass it; add dependencies because whoops.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 const positioner = Object.create(null);
 
 function move(num, code) {
-	if(typeof +num === "number") {
+	if (!Number.isNaN(Number(num))) {
 		process.stdout.write(`\x1b[${Math.round(num)}${code}`);
 	}
 }
@@ -17,35 +17,36 @@ positioner.moveTo = (x = 0, y = 0) => {
 positioner.center = () => {
 	const centerX = Math.round(positioner.columns() / 2);
 	const centerY = Math.round(positioner.rows() / 2);
+
 	return positioner.moveTo(centerX, centerY);
 };
 
 positioner.moveUp = (lines = 0) => {
-	if(lines < 0) {
+	if (lines < 0) {
 		return positioner.moveDown(-lines);
 	}
 	move(lines, "A");
 	return positioner;
 };
 
-positioner.moveDown = (lines) => {
-	if(lines < 0) {
+positioner.moveDown = lines => {
+	if (lines < 0) {
 		return positioner.moveUp(-lines);
 	}
 	move(lines, "B");
-	return positioner;	
+	return positioner;
 };
 
-positioner.moveLeft = (columns) => {
-	if(columns < 0) {
+positioner.moveLeft = columns => {
+	if (columns < 0) {
 		return positioner.moveRight(-columns);
 	}
 	move(columns, "C");
 	return positioner;
 };
 
-positioner.moveRight = (columns) => {
-	if(columns < 0) {
+positioner.moveRight = columns => {
+	if (columns < 0) {
 		return positioner.moveLeft(-columns);
 	}
 	move(columns, "D");
